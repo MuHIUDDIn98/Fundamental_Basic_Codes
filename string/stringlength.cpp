020d5eb9ca7dc686974081d9912e783ca34094fa
anik <muhiuddinanik98@gmail.com>
2023-02-25 01:55:38 +0600

    string

--- string/stringlength.cpp
+++ string/stringlength.cpp
@@ -14,7 +14,17 @@ for(i=0;str[i] !='\0';i++){
 
 }
 
-cout<<"your string lenght is :"<<length;
+cout<<"your string lenght is :"<<length<<endl;
 
+
+char s[]={65,66,67,68,'\0'};
+char t[]={69,70,71,72,73,74,0,75};
+cout<<"charecter array s :"<<s<<endl;
+cout<<"charecter array t :"<<t<<endl;
+
+cout<<"Enter Element in s :";
+cin>>s;
+cout<<"you entered :";
+cout<<s;
 return 0;
 }
