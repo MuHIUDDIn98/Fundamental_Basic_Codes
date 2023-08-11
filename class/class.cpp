2d6341aaa7e859c2e486843e6e76ab307d2d77be
anik <muhiuddinanik98@gmail.com>
2023-08-11 13:30:50 +0600

    change from office

--- class/class.cpp
+++ class/class.cpp
@@ -18,7 +18,7 @@ public:
 main()
 {
  student alim;
-    //test hello test from pc again test
+    //test hello test from pc again test tst from office pc
     alim.id=1214;
     alim.gpa=3.15;
     alim.display();
