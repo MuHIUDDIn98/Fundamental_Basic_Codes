7bc2ed2b536f192decf7ff6a71f33a1de608a00c
anik <muhiuddinanik98@gmail.com>
2023-09-18 21:45:13 +0600

    output converted into string

--- Math functions/DecimalToBinery/DecimalToBinery.cpp
+++ Math functions/DecimalToBinery/DecimalToBinery.cpp
@@ -1,18 +1,25 @@
-#include <iostream>  
-using namespace std;  
+#include <iostream> 
+#include<string>
+
+using namespace std; 
+
 int main()  
 {  
-int a[10], n, i;    
-cout<<"Enter the number to convert: ";    
-cin>>n;    
-for(i=0; n>0; i++)    
-{    
-a[i]=n%2;    
-n= n/2;  
-}    
-cout<<"Binary of the given number : ";    
-for(i=i-1 ;i>=0 ;i--)    
-{    
-cout<<a[i];    
-}    
+    int a[10], n, i;   
+    string num=""; 
+    cout<<"Enter the number to convert: ";    
+    cin>>n;    
+    for(i=0; n>0; i++)    
+    {    
+    a[i]=n%2;    
+    n = n/2;  
+    }    
+    
+    for(i=i-1 ;i>=0 ;i--)    
+    {string ns=to_string(a[i]);
+    num+=ns;
+    } 
+    cout<<"Binary of the given number : ";   
+    cout<<num;
+
 }  
