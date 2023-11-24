ac691b8b188b1a3655da4979e94920e317419402
anik_home_pc <muhiuddinanik98@gmail.com>
2023-11-25 02:13:10 +0600

    header file

---
+++ header files/different file class diclaration/MyFirstProject/include/myfirstclass.h
@@ -0,0 +1,16 @@
+#ifndef MYFIRSTCLASS_H
+#define MYFIRSTCLASS_H
+
+
+class MyFirstClass
+{
+    public:
+        MyFirstClass();
+        void display();
+
+    protected:
+
+    private:
+};
+
+#endif // MYFIRSTCLASS_H
---
+++ header files/different file class diclaration/MyFirstProject/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "myfirstclass.h"
+using namespace std;
+
+int main()
+{
+
+    MyFirstClass obj;
+    obj.display();
+
+    return 0;
+}
