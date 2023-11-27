628e10d9543d227f6b0e3ab0e26481333167c6db
anik_home_pc <muhiuddinanik98@gmail.com>
2023-11-27 23:42:09 +0600

    input getline()

--- Input_Output/Input_Output_cpp/input_output.cpp
+++ Input_Output/Input_Output_cpp/input_output.cpp
@@ -5,6 +5,7 @@ using namespace std; //it is like groupe... cout is located on namespace std
 int main(){
     int a,b;
     double c,d;
+    string f;
     char s[50];
     char g[50];
 
@@ -12,8 +13,11 @@ int main(){
     std::cout<<"test print using std namespace"<<endl;
     cout<<"Enter your numbers :";
     cin>>a>>b;
+    cin.ignore(); //getchar()   //spetial case.. string  after integer must use cin.ignore(), getchar() 
+    getline(cin,f);
     cout<<endl;
     cout<<"a  = "<<a<<" "<<"b = "<<b<<endl;
+    cout<<f<<endl;
     
     //float input
     cout<<"Enter your float numbers :";
