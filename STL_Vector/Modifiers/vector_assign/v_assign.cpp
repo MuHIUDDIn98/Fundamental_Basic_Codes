3f1d87927c8b1e97c5d006851936abfbbb9f156e
anik_home_pc <muhiuddinanik98@gmail.com>
2023-11-29 21:50:27 +0600

    assign vector

---
+++ STL_Vector/Modifiers/vector_assign/v_assign.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int>v = {12,13,14,15,16};
+    vector<int>v2 = {1,2,3};
+    v2 = v;
+    for(int i=0; i<v2.size(); i++){
+        cout<<v2[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
