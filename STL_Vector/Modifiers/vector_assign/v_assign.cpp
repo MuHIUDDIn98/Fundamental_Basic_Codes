dbb62488683c6c35e10b14493a273ba4078a1c10
anik_home_pc <muhiuddinanik98@gmail.com>
2023-11-30 06:57:57 +0600

    Vector input

--- STL_Vector/Modifiers/vector_assign/v_assign.cpp
+++ STL_Vector/Modifiers/vector_assign/v_assign.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     vector<int>v = {12,13,14,15,16};
     vector<int>v2 = {1,2,3};
-    v2 = v;
+    v2 = v; //v.assign(v);
     for(int i=0; i<v2.size(); i++){
         cout<<v2[i]<<" ";
     }
