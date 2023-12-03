d821e30dee9c43e0bf410344fbab8d9f0b52fff4
Muhiuddin anik <muhiuddinanik98@gmail.com>
2023-12-03 18:33:42 +0600

    creating node

---
+++ Link_list_DS_and_it's_Operation/crate_a_node.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+     public:
+     int value;
+     Node* next;
+};
+
+int main(){
+ 
+     Node a,b,c;
+     a.value = 10;
+     a.next = &b;
+     b.next = &c;
+     c.next = NULL;
+     a.next->value = 3; //assigning value
+     (*a.next).value = 103;
+     cout<<b.value<<endl;
+     b.next->value = 15;//assigning value
+     cout<<a.value<<" "<<b.value<<" "<<c.value<<endl;
+     
+    return 0;
+}
