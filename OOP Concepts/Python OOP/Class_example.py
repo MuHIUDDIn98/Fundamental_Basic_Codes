753727083847e154405f34027426c45cec0cd403
anik_home_pc <muhiuddinanik98@gmail.com>
2024-02-16 14:25:39 +0600

    class object costructor  in python

---
+++ OOP Concepts/Python OOP/Class_example.py
@@ -0,0 +1,8 @@
+class Employee:
+    def printEmpDetails(self, name , id):
+        print('Emp Name :', name)
+        print('Emp ID :', id)
+        
+        
+emp1 = Employee()
+emp1.printEmpDetails("anik",3444)
