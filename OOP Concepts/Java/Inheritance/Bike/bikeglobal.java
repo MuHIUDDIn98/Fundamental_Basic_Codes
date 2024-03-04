8c147db6cb277503f7356d799bc8d3352ab45d6c
Muhiuddin anik <muhiuddinanik98@gmail.com>
2024-03-05 01:23:22 +0600

    Exception handling, inheritance

---
+++ OOP Concepts/Java/Inheritance/Bike/bikeglobal.java
@@ -0,0 +1,15 @@
+//package Bike;
+
+public class bikeglobal {
+    String color;
+    double weight;
+    String Model;
+    double Height;
+    String Breaking;
+    public void startBike(){
+        System.out.println("kik to start the bike");
+    }
+    public void stopBike(){
+        System.out.println("Stop Bike");
+    }
+}
---
+++ OOP Concepts/Java/OOP/bankmulticlass/Bank.java
@@ -0,0 +1,10 @@
+public class Bank {
+    String name;
+    int accountbalance;
+    Members mem;
+    public int interestgained(int increment){
+        accountbalance = accountbalance + increment;
+        return accountbalance;
+
+    }
+}
