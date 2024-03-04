cb8ca1958bd9393d18b5dda48fecfdff15f4f242
Muhiuddin anik <muhiuddinanik98@gmail.com>
2024-03-04 21:13:23 +0600

    Array in java

--- 1.Array_DS_and_its_operatons/Array_And_its_DS_java/Array/Array.java
+++ 1.Array_DS_and_its_operatons/Array_And_its_DS_java/Array/Array.java
@@ -1,8 +1,11 @@
 public class Array {
-  
+     
     public static void main(String args[]){
+    int rows = 3;
+     int cols = 3;
     String[] s = new String[5];
     String[] stringArray = new String[10];//creating array 
+    int table[][] = new int[rows][cols];
 
     s = new String[] {"shihab","reja","subrata","Rayhan"};   
     
