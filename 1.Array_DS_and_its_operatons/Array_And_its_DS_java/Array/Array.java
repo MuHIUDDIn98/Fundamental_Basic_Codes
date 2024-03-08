6f9f36393142828d3a6058fed6d425a07b0e32d3
Muhiuddin anik <muhiuddinanik98@gmail.com>
2024-03-08 07:39:55 +0600

    basi Array in java

--- 1.Array_DS_and_its_operatons/Array_And_its_DS_java/Array/Array.java
+++ 1.Array_DS_and_its_operatons/Array_And_its_DS_java/Array/Array.java
@@ -1,14 +1,32 @@
 public class Array {
      
     public static void main(String args[]){
+
+    int[] arr = new int[5];
+
+    String[] catagory ={"phone","Accessories","home appliance"};
+    //2D matrix
     int rows = 3;
      int cols = 3;
-    String[] s = new String[5];
-    String[] stringArray = new String[10];//creating array 
     int table[][] = new int[rows][cols];
+    
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
+    arr[3] = 4;
+    arr[4] = 5;
 
+    int[] arr2 = {1,2,3,4,5};
+
+    for(int i=0; i<5; i++){
+
+        System.out.println(arr[i]);
+    }
+
+    String[] s = new String[5];
     s = new String[] {"shihab","reja","subrata","Rayhan"};   
     
+    String[] stringArray = new String[10];//creating array  in heap
     stringArray = new String[] {"Amit","Rayhan","saikat","anik"}; //initialization of array
     for(int i=0; i<3; i++){
         System.out.println(s[i]);
