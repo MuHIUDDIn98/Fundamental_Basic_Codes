29360b1b0db2111b9d59ecd0b9a3ed3b6c6fa191
Muhiuddin anik <muhiuddinanik98@gmail.com>
2024-04-04 11:32:11 +0600

    args

---
+++ 1.Basic concepts/4.Differnt type of Loops/Loop_python/sub_string_checker.py
@@ -0,0 +1,8 @@
+s = input("Enter your string :")
+
+
+if 'an' in s:
+    print("present ")
+    
+else:
+    print("Not present")
---
+++ 1.Basic concepts/5.function/Function_Python/Arguments_key_arguments/args1.py
@@ -0,0 +1,16 @@
+def arg_test(name,*args):
+    print(name)
+    print(*args)
+    print(f'args print {args[1]}')
+    sum = 0
+    for i in args:
+        sum = i+sum
+        
+    return sum
+        
+   
+
+
+sum = arg_test("anik",32,22,44)
+print(f'sum of all arges {sum}')
+arg_test("ashik",45,33)
