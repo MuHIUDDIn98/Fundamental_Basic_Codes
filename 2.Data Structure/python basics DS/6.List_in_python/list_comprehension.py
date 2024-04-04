e4841261825a8f56e8a9cf91b40bdc6597261916
Muhiuddin anik <muhiuddinanik98@gmail.com>
2024-04-04 11:59:05 +0600

    python list comprehension

--- 2.Data Structure/python basics DS/6.List_in_python/list_comprehension.py
+++ 2.Data Structure/python basics DS/6.List_in_python/list_comprehension.py
@@ -1,3 +1,15 @@
+num =[]
+
+num = [i for i in range(3,12)]
+print(num)
+
+num = [i**2 for i in range(3,12)]
+print(num)
+
+num = [[i,i**2] for i in range(3,12)]
+print(num)
+
+
 numbers = [12,13,14,45,656,7,8,89]
 
 
@@ -12,3 +24,15 @@ ages = [37,38,39]
 
 pairs = [(name,age) for name in names for age in ages]
 print(pairs)
+
+
+print("combination of 7*7")
+
+combination = [[i,j] for i in range(3) for j in range(3)]
+print(combination)
+# for i in range(7):
+#     for j in range(7):
+#         combination.append([i,j])
+
+# print(combination)
+
