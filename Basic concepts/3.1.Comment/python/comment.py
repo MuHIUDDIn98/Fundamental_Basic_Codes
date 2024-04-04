c3dc9240371e510ebe93938b91d24b78390d8ed9
Muhiuddin anik <muhiuddinanik98@gmail.com>
2024-04-04 10:08:00 +0600

    python_variable data type and commenting

---
+++ Basic concepts/3.1.Comment/python/comment.py
@@ -0,0 +1,11 @@
+# print(age, type(age)) A single line Comment . Press Ctrl+ /
+
+
+#multiline comment
+""" 
+Multi Line Comment Press Alt + Shift + A
+print(name, type(name))
+print(is_single, type(is_single))
+print(interest, type(interest))
+
+"""
