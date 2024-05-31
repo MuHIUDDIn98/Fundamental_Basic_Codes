5792e18ac749de0f66fe34ce887bf3bc11f46a31
anikLinux <muhiuddinanik98@gmail.com>
2024-05-31 09:57:08 +0600

    Dictionery

--- 2.Data Structure/python basics DS/8.Dictionery/dictionery_iteration.py
+++ 2.Data Structure/python basics DS/8.Dictionery/dictionery_iteration.py
@@ -1,13 +1,33 @@
 employeeData = {"name":"Anik","number":"01521327657","DOB":1998}
+emp_key_list=[]
+emp_val_list=[]
 
 employeeData["Department"] = "IT"
 employeeData["Designation"] = "SQA"
 employeeData["Home address"] = "Mogbazar"
-#looping through all keys
-for item in employeeData.keys():
-    print(employeeData[item])
 
+print("All employeeData values :")
+#looping through all keys and print  values
+for item in employeeData.keys():
+    emp_val_list.append(employeeData[item])
+    emp_key_list.append(item)
+    print(employeeData[item],end=" ")
+
+print()
+print(emp_key_list)
+print(emp_val_list)
+print()
+print()
+
+print("iterating all values: ")
+for _,val in employeeData.items():
+    print(val,end=" ")
+
+
+print()
+print()
 #looping through all key and value
+print("All key values from dictionery :")
 for key,value in employeeData.items():
     print(f'key:{key}, value:{value}')
     
