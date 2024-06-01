cf4bbe7b93426cdf05a01730b2057e14c93df373
Muhiuddin anik <muhiuddinanik98@gmail.com>
2024-06-01 18:14:41 +0600

    range for loop reverse , forword

---
+++ 1.Basic concepts/4.Differnt type of Loops/Loop_python/For loop/numeric.py
@@ -0,0 +1,18 @@
+numeric = 15
+
+for iteration in range(numeric,2,-2):
+    print(f'your current count is {iteration}     (2 steps)')
+    
+print()
+
+for iteration in range(numeric,22,1):
+    print(f'your current count is {iteration}     (2 steps)')
+    
+print()
+for iteration in range(numeric,5,-1):
+    print(f'your current count is {iteration}    (1 steps)')
+print()
+for iteration in range(numeric,3,-3):
+    print(f'your current count is {iteration}     (3 steps)')
+
+print()
