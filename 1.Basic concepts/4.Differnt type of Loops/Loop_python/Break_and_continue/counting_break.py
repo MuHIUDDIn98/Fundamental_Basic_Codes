f76f9f957032cdfc534ae1f1e26c5d5eb6d67af8
Muhiuddin anik <muhiuddinanik98@gmail.com>
2024-06-01 18:08:07 +0600

    Break and continue

---
+++ 1.Basic concepts/4.Differnt type of Loops/Loop_python/Break_and_continue/counting_break.py
@@ -0,0 +1,6 @@
+x=15
+while x > 0:
+    print(f'Your current count is :{x}')
+    x -= 1
+    if x == 5:
+        break
---
+++ 1.Basic concepts/4.Differnt type of Loops/Loop_python/While_loop/while_loop_counter.py
@@ -0,0 +1,4 @@
+x=5
+while x > 0:
+    print(f'Your current count is :{x}')
+    x -= 1
