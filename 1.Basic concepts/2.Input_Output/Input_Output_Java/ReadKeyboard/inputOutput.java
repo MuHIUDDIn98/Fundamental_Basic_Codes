cb09c6838a282b3aca648a23f4172a78159de77f
Muhiuddin anik <muhiuddinanik98@gmail.com>
2024-10-02 14:00:18 +0600

    java readfrom keyboard

---
+++ 1.Basic concepts/2.Input_Output/Input_Output_Java/ReadKeyboard/inputOutput.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class inputOutput {
+    public static void main(String args[]){
+        //scanner object creation
+        Scanner sc = new Scanner(System.in);
+
+        int x;
+        int y;
+
+        x = sc.nextInt();
+        y= sc.nextInt();
+
+        int  z = x+y;
+
+        System.out.println("Sum of x and y :"+z);
+
+
+    }
+    
+
+    
+}
