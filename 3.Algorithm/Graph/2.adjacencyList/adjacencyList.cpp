ffabdc1b15a08b6246b27f30c8c47896b9f04c4d
Muhiuddin anik <muhiuddinanik98@gmail.com>
2024-10-10 13:55:43 +0600

    adjacency list check connected nodes

--- 3.Algorithm/Graph/2.adjacencyList/adjacencyList.cpp
+++ 3.Algorithm/Graph/2.adjacencyList/adjacencyList.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main(){
     int n,e;
+    cout<<"Enter your nodes and Edges count"<<endl;
     cin>>n>>e;
+    cout<<"entered node count :"+to_string(n)+" edges :"+to_string(e)<<endl;
     vector<int> AdCL[n];
+    cout<<"Enter all '"+to_string(e)+"' edges "<<endl;
     while(e--){
         int a,b;
         cin>>a>>b;
@@ -12,7 +15,15 @@ int main(){
         AdCL[b].push_back(a);
 
     }
+    int node;
+    cout<<"Enter your querry node"<<endl;
+    cin>>node;
+    cout<<"printing n nodes connected nodes:"<<endl;
+    for(int i=0; i<AdCL[node].size();i++){
 
+        cout<<AdCL[node][i]<<" ";
+    }
+    cout<<endl;
 
     cout<<"=========printing 3 index's vector========="<<endl;
     for(int i=0; i<AdCL[3].size();i++){
