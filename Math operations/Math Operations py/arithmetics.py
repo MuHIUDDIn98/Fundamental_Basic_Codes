586f41ff545c0067c6a1eaeee4dce1c7237c9360
MuhiuddinHomeDesktop <muhiuddinanik98@gmail.com>
2025-03-21 23:04:32 +0600

    arithmetics.py

---
+++ Math operations/Math Operations py/arithmetics.py
@@ -0,0 +1,8 @@
+print(f'2+2 = {2+2}')
+print(f'2-2 = {2-2}')
+print(f'2*2 = {2*2}')
+print(f'10/2 = {10/2}')
+print(f'5/2 = {5/2}')
+print(f'5//2 = {5//2}') #remainder
+print(f' 10%3 = {10%3}') #mod
+print(f' 2^3 = {2**3}') #power
