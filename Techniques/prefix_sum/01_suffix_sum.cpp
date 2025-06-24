876910861221f9978bc103bc0a287952df184397
MuhiuddinHomeDesktop <muhiuddinanik98@gmail.com>
2025-06-24 23:50:06 +0600

    prefix sum , suffix sum , equilibrium indx check

---
+++ Techniques/prefix_sum/01_suffix_sum.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+   int n;
+   cin >> n;
+   vector<int> v(n);
+
+   for (int i = 0; i < n; i++)
+   {
+      cin >> v[i];
+   }
+
+   vector<int> prefix(n);
+   prefix[0] = v[0];
+
+   for (int i = 1; i < n; i++)
+   {
+      prefix[i] = prefix[i - 1] + v[i];
+   }
+
+   for (int i = 0; i < n; i++)
+   {
+
+      cout << prefix[i] << " ";
+   }
+
+   cout << endl;
+
+   // Suffix sum
+   vector<int> suffix(n);
+   suffix[n - 1] = v[n - 1];
+   for (int i = n - 2; i >= 0; i--)
+   {
+      suffix[i] = suffix[i + 1] + v[i];
+   }
+
+   for (int i = 0; i < n; i++)
+   {
+
+      cout << suffix[i] << " ";
+   }
+
+   return 0;
+}
